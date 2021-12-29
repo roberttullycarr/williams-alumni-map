f644ed200820fd482f7679e521701f56994204ae
robertcarr <roberttullycarr@gmail.com>
2021-12-29 14:36:12 +0100

    cleared logs and errors

--- components/mapbox-map.tsx
+++ components/mapbox-map.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from 'react';
 import PopUp from './popup';
 import Markers from './Markers';
 import { AlumniType, FilterOptions } from '../Interfaces';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 interface Props {
